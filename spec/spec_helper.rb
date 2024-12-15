5576b127c2dd01c5530ff093f888a369807ee983
Eric Beland <ebeland@gmail.com>
2024-12-15 15:17:45 -0500

    Cleanup examples and make helpers off during self-tests

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,14 +2,4 @@
 require 'rspec'
 require 'enhanced_errors'
 
-RSpec.configure do |config|
-  config.before(:example) do |_example|
-    EnhancedErrors.enhance_exceptions!
-    EnhancedErrors.start_rspec_binding_capture
-  end
-
-  config.after(:example) do |example|
-    EnhancedErrors.override_exception_message(example.exception, EnhancedErrors.stop_rspec_binding_capture)
-  end
-end
 
